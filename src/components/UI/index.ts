67717ed2ed4477fef1f897a6022dec07c6379c6d
Maksim <mgandronov2017@gmail.com>
2022-06-08 16:03:16 +0500

    добавляет файл импорта UI компонентов

---
+++ src/components/UI/index.ts
@@ -0,0 +1,6 @@
+import LinkTab from './LinkTab';
+import MenuIconButton from './MenuIconButton';
+import ShowMoreButton from './ShowMoreButton';
+import SortButton from './SortButton';
+
+export { LinkTab, MenuIconButton, ShowMoreButton, SortButton };
