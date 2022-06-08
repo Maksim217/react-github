e4ffc8da98d7a5edefb06f94c7d7db9f77e66e32
Maksim <mgandronov2017@gmail.com>
2022-06-08 16:03:23 +0500

    добавляет файл импорта компонентов

---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+import App from './App';
+import Navbar from './Navbar';
+import DrawerMenu from './DrawerMenu';
+import MyProfile from './MyProfile';
+import RepoList from './RepoList';
+import RepoItem from './RepoItem';
+import FollowingItem from './FollowingItem';
+import FollowingList from './FollowingList';
+import PageTitle from './PageTitle';
+import Loader from './Loader';
+
+export {
+  App,
+  Navbar,
+  DrawerMenu,
+  MyProfile,
+  RepoList,
+  RepoItem,
+  FollowingList,
+  FollowingItem,
+  PageTitle,
+  Loader,
+};
