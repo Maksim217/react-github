9dbbdc10f60cefacb5bab3ffd1e90f4d700c77c1
mgandronov <mgandronov2017@gmail.com>
2022-08-20 17:47:22 +0500

    обновляет импорты компонентов в файле index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,9 +3,12 @@ import Navbar from './Navbar';
 import DrawerMenu from './DrawerMenu';
 import MyProfile from './MyProfile';
 import RepoList from './RepoList';
-import RepoItem from './RepoItem';
-import FollowingItem from './FollowingItem';
+import RepoItem from './RepoList/RepoItem';
+import FollowingItem from './FollowingList/FollowingItem';
 import FollowingList from './FollowingList';
+import UserList from './UserList';
+import UserItem from './UserList/UserItem';
+import MessageSection from './MessageSection';
 import PageTitle from './PageTitle';
 import Loader from './Loader';
 
@@ -17,7 +20,10 @@ export {
   RepoList,
   RepoItem,
   FollowingList,
+  MessageSection,
   FollowingItem,
+  UserList,
+  UserItem,
   PageTitle,
   Loader,
 };
